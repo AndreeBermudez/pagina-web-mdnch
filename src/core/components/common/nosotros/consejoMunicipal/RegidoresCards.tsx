f48a185f5478752137569ae95cbce2d3cbcc1571
Abel Pelaez <abelpelaez16@gmail.com>
2025-06-02 15:47:43 -0500

    cositas

--- src/core/components/common/nosotros/consejoMunicipal/RegidoresCards.tsx
+++ src/core/components/common/nosotros/consejoMunicipal/RegidoresCards.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 
 interface Regidor {
   id: number;
--- src/core/components/common/nosotros/directorio/DirectorioCards.tsx
+++ src/core/components/common/nosotros/directorio/DirectorioCards.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import { Mail, ArrowLeft, ArrowRight } from 'lucide-react';
 import type { Funcionario } from './types';
 
